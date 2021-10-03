fdda910146c2907555e0c9aa0753e2fd2b84e5db
Harshit <dubeyharshit746@gmail.com>
2021-10-03 17:17:25 +0530

    inital commit

---
+++ sum.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define vi vector<int>
+#define vvi vector<vi>
+#define pii pair<int, int>
+#define vii vector<pii>
+#define si stack<int>
+#define qi queue<int>
+#define ll long long
+#define rep(i, a, b) for (int i = a; i < b; i++)
+
+int main(){
+  int a = 3, b = 2;
+  cout<<a+b<<endl;
+  return 0;
+}
