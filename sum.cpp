4583e176a32cc36cf3a724d1d3884293879b8bb6
Harshit <dubeyharshit746@gmail.com>
2021-10-03 18:23:45 +0530

    Changed the code format

--- sum.cpp
+++ sum.cpp
@@ -1,13 +1,5 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-#define vi vector<int>
-#define vvi vector<vi>
-#define pii pair<int, int>
-#define vii vector<pii>
-#define si stack<int>
-#define qi queue<int>
-#define ll long long
-#define rep(i, a, b) for (int i = a; i < b; i++)
 
 int main(){
   int a = 3, b = 2;
